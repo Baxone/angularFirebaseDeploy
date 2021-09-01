22ae401c205ca9d3676176e3588e54836723b824
Juan Antonio Pérez Jarillo <jantonio.perez@estudioclandestino.es>
2021-09-01 12:16:45 +0200

    house db creado e importado

--- src/app/services/houses.service.ts
+++ src/app/services/houses.service.ts
@@ -1,25 +1,16 @@
-import { Injectable } from '@angular/core',
+import { Injectable } from '@angular/core';
 import { House } from '../interfaces/house.interface';
+import { HOUSES } from './houses.db';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class HousesService {
 
-  arrHouses: House[];
+  arrHouses: House[] = [];
 
   constructor() {
-    this.arrHouses = [{
-      id: 1,
-      titulo: "Bonita casa adosada",
-      direccion: "Calle del Almendro 17, 28090",
-      ciudad: 'Madrid',
-      numerohabitaciones: 3,
-      propietario: 'Don Ramiro',
-      disponibilidad: true,
-      foto: 'https://media.yaencontre.com/img/photo/w380/6385/6385-29962224-919894441.jpg',
-      lat: 1.1,
-      long: 2.2
-    }]
+    this.arrHouses = HOUSES;
   }
 }
